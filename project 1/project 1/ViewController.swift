cc07546c96705f2d2194ad0e6a29bb299b76ab7e
Jerry Ye <jerryye@Jerrys-MBP-2.cable.rcn.com>
2020-03-22 10:15:10 -0400

    finished day 1

---
+++ project 1/project 1/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  project 1
+//
+//  Created by Jerry Ye on 3/22/20.
+//  Copyright © 2020 Jerry Ye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items{
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+
+}
+
