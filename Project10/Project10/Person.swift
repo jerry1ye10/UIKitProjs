d56ee060019e03f9d5efbf3f0085af56488eba96
Jerry Ye <jerry1ye10@gmail.com>
2020-04-16 13:31:27 -0400

    added project 10: collection views

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Jerry Ye on 4/16/20.
+//  Copyright © 2020 Jerry Ye. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
---
+++ Project10/Project10/PersonCellCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCellCollectionViewCell.swift
+//  Project10
+//
+//  Created by Jerry Ye on 4/16/20.
+//  Copyright © 2020 Jerry Ye. All rights reserved.
+//
+
+import UIKit
+
+class PersonCellCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
